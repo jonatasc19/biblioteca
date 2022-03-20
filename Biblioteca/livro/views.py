79d21a6a7953da7ea228c7fc8a15e1f4ce9ad11c
Jonatas <jonatasnascimento37@icloud.com>
2022-03-20 14:05:07 -0300

    commit inicial

---
+++ Biblioteca/livro/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from usuarios.models import Usuario
+from .models import Livros
+
+# Create your views here.
+
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        livros = Livros.objects.filter(usuario = usuario)
+        return render(request, 'home.html', {'livros': livros})
+    else:
+        return redirect('/auth/login/?status=2')
+
+def ver_livros(request, id):
+    livros = Livros.objects.get(id=id)
+    return render(request, 'ver_livro.html', {'livro': livros})
