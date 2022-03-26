0fa7c6375f428a0042fc221d18521b26797dc993
Jefferson169 <jeffersonnathan169@gmail.com>
2022-03-26 12:34:48 -0300

    Implementação de algumas regras, nova tabela, bd

--- Biblioteca/livro/models.py
+++ Biblioteca/livro/models.py
@@ -5,11 +5,10 @@ from usuarios.models import Usuario
 class Categoria (models.Model):
     nome = models.CharField(max_length = 100)
     descricao = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
           return self.nome
-
-
 
 class Livros(models.Model):
     nome = models.CharField(max_length = 100)
@@ -30,3 +29,14 @@ class Livros(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Emprestimos(models.Model):
+    
+    #nome_emprestado_anonimo = models.CharField(max_length = 100, blank = True)
+    nome_emprestado = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    data_emprestimo = models.DateField(blank = True, null = True)
+    data_devolucao = models.DateField(blank = True, null = True)
+    livro = models.ForeignKey(Livros, on_delete=models.DO_NOTHING)
+    def __str__(self) -> str:
+        return f"{self.nome_emprestado} | {self.livro}"
--- Biblioteca/livro/views.py
+++ Biblioteca/livro/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from usuarios.models import Usuario
-from .models import Livros
+from .models import Emprestimos, Livros, Categoria
 
 # Create your views here.
 
@@ -18,7 +18,9 @@ def ver_livros(request, id):
    if request.session.get('usuario'):
        livros = Livros.objects.get(id = id)
        if request.session.get('usuario') == livros.usuario.id:
-            return render(request, 'ver_livro.html', {'livro': livros})
+            categoria_livro = Categoria.objects.filter(usuario_id = request.session.get('usuario')) 
+            emprestimos = Emprestimos.objects.filter(livro = livros)
+            return render(request, 'ver_livro.html', {'livro': livros,'categoria_livro':categoria_livro, 'emprestimos':emprestimos})
        else:
           return HttpResponse('Esse livro não é seu')
    return redirect('/auth/login/?status=2')
