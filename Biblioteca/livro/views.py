2a06b1ed90bbc1076c40e1248a215a594e999a25
Jefferson169 <jeffersonnathan169@gmail.com>
2022-03-28 20:05:00 -0300

    views.py

--- Biblioteca/livro/views.py
+++ Biblioteca/livro/views.py
@@ -9,7 +9,7 @@ def home(request):
     if request.session.get('usuario'):
         usuario = Usuario.objects.get(id = request.session['usuario'])
         livros = Livros.objects.filter(usuario = usuario)
-        return render(request, 'home.html', {'livros': livros})
+        return render(request, 'home.html', {'livros': livros,'usuario_logado':request.session.get('usuario')})
     else:
         return redirect('/auth/login/?status=2')
 
@@ -20,7 +20,7 @@ def ver_livros(request, id):
        if request.session.get('usuario') == livros.usuario.id:
             categoria_livro = Categoria.objects.filter(usuario_id = request.session.get('usuario')) 
             emprestimos = Emprestimos.objects.filter(livro = livros)
-            return render(request, 'ver_livro.html', {'livro': livros,'categoria_livro':categoria_livro, 'emprestimos':emprestimos})
+            return render(request, 'ver_livro.html', {'livro': livros,'categoria_livro':categoria_livro, 'emprestimos':emprestimos, 'usuario_logado':request.session.get('usuario')})
        else:
           return HttpResponse('Esse livro não é seu')
    return redirect('/auth/login/?status=2')
